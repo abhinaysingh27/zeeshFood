89ce264067e1491cd3462fb647a1f5ca5c651c2a
abhinaysingh27 <abhinay.chiku1234@gmail.com>
2025-08-27 04:08:47 +0530

    Initial commit

---
+++ MERN_STACK_RESTAURANT_RESERVATION-main/frontend/src/Pages/Success/Success.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { HiOutlineArrowNarrowRight } from "react-icons/hi";
+
+const Success = () => {
+  const [countdown, setCountdown] = useState(10);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (countdown === 0) {
+      navigate("/"); 
+      return;
+    }
+
+    const timer = setTimeout(() => {
+      setCountdown((prev) => prev - 1);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [countdown, navigate]);
+
+  return (
+    <section className="notFound">
+      <div className="container">
+        <img src="/sandwich.png" alt="success" />
+        <h1>Redirecting to Home in {countdown} seconds...</h1>
+        <Link to={"/"}>
+          Back to Home <HiOutlineArrowNarrowRight />
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default Success;
---
+++ MERN_STACK_RESTAURANT_RESERVATION-main/frontend/src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">ZEESHU</div>
+          <div className="right">
+            <p>Boring road,Patna</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By ABHINAY SINGH</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
